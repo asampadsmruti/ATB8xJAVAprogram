a7fdd4898bd450faea4db0990a5155e08f9af10d
at8x <sampadsmrutiranjan48@gmail.com>
2024-09-06 16:57:22 +0530

    first command

--- src/ex04092024/Lab001.java
+++ src/ex04092024/Lab001.java
@@ -1,4 +1,10 @@
 package ex04092024;
 
 public class Lab001 {
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
 }
+
+
--- src/ex04092024/Lab002.java
+++
@@ -1,4 +0,0 @@
-package ex04092024;
-
-public class Lab002 {
-}
